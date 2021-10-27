74c6d7d4307febfed9e75b0d861eb43b90501b31
soo <ella1118@naver.com>
2021-10-27 23:15:14 +0900

    add fill and resetBtn

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const colors=document.getElementsByClassName("jsColor");
 const range=document.getElementById("jsRange");
 const mode=document.getElementById("jsMode");
 const reset=document.getElementById("jsReset");
+const saveBtn=document.getElementById("jsSave");
 
 const INITIAL_COLOR="#black";
 const CANVAS_SIZE=700;
@@ -11,6 +12,8 @@ const CANVAS_SIZE=700;
 canvas.width=CANVAS_SIZE;
 canvas.height=CANVAS_SIZE;
 
+ctx.fillStyle="white";
+ctx.fillRect(0,0,CANVAS_SIZE,CANVAS_SIZE);
 ctx.strokeStyle=INITIAL_COLOR;
 ctx.fillStyle=INITIAL_COLOR;
 ctx.lineWidth=2.5;
@@ -68,10 +71,18 @@ function handleCanvasClick(){
     }
 }
     
-
-
 function resetBtn(event){
     window.location.reload();
+}
+
+function handleCM(event){
+    event.preventDefault();
+}
+
+function handleSaveClick(){
+    const image=canvas.toDataURL("image/jpeg");
+    const link =document.createElement("a"); 
+    link.download=
 }
 
 
@@ -81,6 +92,7 @@ if(canvas){
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
     canvas.addEventListener("click",handleCanvasClick);
+    canvas.addEventListener("contextmenu",handleCM);
 }
 
 Array.from(colors).forEach(color => 
@@ -96,4 +108,8 @@ if(mode){
 };
 if (reset) {
     reset.addEventListener("click", resetBtn);
+}
+
+if (saveBtn){
+    saveBtn.addEventListener("click",handleSaveClick);
 }
